aeafdcee5fb5090e56a8f5900bc00841c07f2bbe
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2024-03-11 21:35:12 -0300

    Login Function

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 
 const Login = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const auth = getAuth();
+
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        console.log("succes");
+      })
+      .catch((error) => {
+        console.log("failed");
+      });
+  };
+
   return (
     <div>
         <h1>Login</h1>
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type="submit" value="Login" onClick={handleLogin} />
     </div>
   );
 };
