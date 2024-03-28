b65baea7c6e798dab40ea2b178a4808905ee4be8
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2024-03-28 15:39:41 -0300

    Filtro por genero
    Agora, ao chamar a função de carregar os filmes, é possivel passar o genero para buscar somente os daqueles gêneros nas sessões

--- src/Components/MoviesList/MoviesList.jsx
+++ src/Components/MoviesList/MoviesList.jsx
@@ -4,15 +4,28 @@ import { db } from '../../App.js';
 import Carousel from '../Carousel/Carousel.jsx';
 import './MoviesList.css';
 
-const MoviesList = ({titulo}) => {
+const MoviesList = ({titulo, genero}) => {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
         const loadMovies = async () => {
             try {
-                const querySnapshot = await getDocs(collection(db, "Movies"));
-                const moviesData = querySnapshot.docs.map(doc => doc.data());
-                setMovies(moviesData);
+                if (genero) {
+                    const querySnapshot = await getDocs(collection(db, "Movies"));
+                    const moviesData = [];
+                    querySnapshot.forEach((doc) => {
+                        const data = doc.data();
+                        if (Array.isArray(data.genero) && data.genero.includes(genero)) {
+                            moviesData.push(data);
+                        }
+                    });
+                    setMovies(moviesData);
+                }
+                else {
+                    const querySnapshot = await getDocs(collection(db, "Movies"));
+                    const moviesData = querySnapshot.docs.map(doc => doc.data());
+                    setMovies(moviesData);
+                }
             } catch (error) {
                 console.error("Erro ao carregar filmes:", error);
             }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './Home.css';
 import MoviesList from '../../Components/MoviesList/MoviesList';
 
 const Home = () => {
   return ( 
     <>
-      <MoviesList titulo="Suspense"/>
-      <MoviesList titulo="Drama"/>
-      <MoviesList titulo="Romance"/>
+      <MoviesList titulo="Todos os filmes"/>
+      <MoviesList titulo="Suspense" genero="Suspense"/>
+      <MoviesList titulo="Drama" genero="Drama"/>
+      <MoviesList titulo="Romance" genero="Romance"/>
     </>
   );
 };
