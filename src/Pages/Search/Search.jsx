ccf8374d24dde50f1e640feb39d5b50c94bd88d5
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2024-05-14 20:49:27 -0300

    Pagina de Pesquisa

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 import Menu from '../../Components/Menu/Menu';
+import MovieCard from '../../Components/MovieCard/MovieCard';
 
 const Search = () => {
-  return ( 
-    <>  
-      <Menu />
-    </>
-  );
+    const [search, setSearch] = useState('');
+    const [responseData, setResponseData] = useState('');
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+
+        fetch(`https://www.omdbapi.com/?apikey=b23d4975&t=${search}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setResponseData(data);
+            })
+            .catch(error => console.error(error));
+    }
+
+    return (
+        <>
+            <Menu />
+            <section className="sectionSearch">
+                <nav className="navSearch">
+                    <input type="text" className="inputSearch" value={search} 
+                        placeholder='Digite o nome do filme'
+                        onChange={e => setSearch(e.target.value)} />
+                    <input type="submit" className="buttonSearch" 
+                        value="Buscar" onClick={handleSearch} />
+                </nav>
+                {responseData && (
+                    <MovieCard poster={responseData.Poster} nome={responseData.Title} className="movieCardSearch" />
+                )}
+            </section>
+        </>
+    );
 };
 
 export default Search;
